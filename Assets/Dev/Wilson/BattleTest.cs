e244008cff1548a9209a95e8aa720b927ecee6c8
lin900822 <wilson24386433@gmail.com>
2024-02-28 13:23:04 +0800

    [Add] 簡單的ECS

---
+++ Assets/Dev/Wilson/BattleTest.cs
@@ -0,0 +1,62 @@
+﻿using Game.Battle;
+using Game.Battle.Entities;
+using UnityEngine;
+
+namespace Dev.Wilson
+{
+    public class BattleTest : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector2 _entity1;
+
+        [SerializeField]
+        private float _radius1;
+
+        [SerializeField]
+        private Vector2 _entity2;
+        [SerializeField]
+        private float _radius2;
+
+        private BattleEntity _battleEntity1;
+        private BattleEntity _battleEntity2;
+
+        private EntityManager _entityManager;
+
+        private void Awake()
+        {
+            _entityManager = new EntityManager();
+
+            _battleEntity1 = new BattleEntity()
+            {
+                Name = "BattleEntity 1"
+            };
+            _battleEntity1.BattleComponent.BattleRadius = _radius1;
+            _battleEntity1.TransformComponent.PosX = (int)_entity1.x;
+            _battleEntity1.TransformComponent.PosY = (int)_entity1.y;
+            
+            _battleEntity2 = new BattleEntity()
+            {
+                Name = "BattleEntity 2"
+            };
+            _battleEntity2.BattleComponent.BattleRadius = _radius2;
+            _battleEntity2.TransformComponent.PosX      = (int)_entity2.x;
+            _battleEntity2.TransformComponent.PosY      = (int)_entity2.y;
+            
+            _entityManager.TryAddEntity(_battleEntity1);
+            _entityManager.TryAddEntity(_battleEntity2);
+        }
+
+        private void Update()
+        {
+            _battleEntity1.BattleComponent.BattleRadius = _radius1;
+            _battleEntity1.TransformComponent.PosX      = (int)_entity1.x;
+            _battleEntity1.TransformComponent.PosY      = (int)_entity1.y;
+            
+            _battleEntity2.BattleComponent.BattleRadius = _radius2;
+            _battleEntity2.TransformComponent.PosX      = (int)_entity2.x;
+            _battleEntity2.TransformComponent.PosY      = (int)_entity2.y;
+            
+            _entityManager.Update();
+        }
+    }
+}
---
+++ Assets/Scripts/Framework/Common/Logger.cs
@@ -0,0 +1,58 @@
+﻿namespace Framework.Common
+{
+    public static class Log
+    {
+        private static ILog _log = new UnityConsoleLog();
+
+        public static void Debug(string message)
+        {
+            _log.Debug(message);
+        }
+
+        public static void Info(string message)
+        {
+            _log.Info(message);
+        }
+
+        public static void Warn(string message)
+        {
+            _log.Warn(message);
+        }
+
+        public static void Error(string message)
+        {
+            _log.Error(message);
+        }
+    }
+
+    public interface ILog
+    {
+        void Debug(string message);
+        void Info(string  message);
+        void Warn(string  message);
+        void Error(string message);
+    }
+
+    public class UnityConsoleLog : ILog
+    {
+        public void Debug(string message)
+        {
+            UnityEngine.Debug.Log($"[Debug] {message}");
+        }
+
+        public void Info(string message)
+        {
+            UnityEngine.Debug.Log($"[Info] {message}");
+        }
+
+        public void Warn(string message)
+        {
+            UnityEngine.Debug.LogWarning($"[Warn] {message}");
+        }
+
+        public void Error(string message)
+        {
+            UnityEngine.Debug.LogError($"[Error] {message}");
+        }
+    }
+}
